d08cf8ea96bae257a1cf79e444dbeda958567a71
joaoveasey <jovepedro@gmail.com>
2023-12-29 16:21:09 -0300

    #

---
+++ ArrayList_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+Console.WriteLine("ArrayList 2\n");
+//podemos utilizar metodos do arraylist:
+//add - permite incluir elementos no final do arraylist
+//insert - permite incluir um elemento em uma posição qualquer usando um índice específico => Insert(int index, Object value);
+
+var lista1 = new ArrayList() { "Maria", 5, true, " ", null };
+
+//lista.Add(3.5);
+
+//lista.Insert(2, "Paulo");
+
+//-----------podemos adicionar coleção de elementos-----
+
+int[] array = { 1, 2, 3, 4, 5, 6 };
+
+lista1.AddRange(array);
+
+
+Console.ReadKey();
