52f84637948b39442d6b4db3ab292286a74e0494
joaoveasey <jovepedro@gmail.com>
2024-01-04 22:12:30 -0300

    #

---
+++ List3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("List - Métodos Find\n");
+
+List<string> frutas = new() 
+{ 
+    "Uva", "Banana", "Pera", "Maçã", "Abacate", "Laranja", "Morango" 
+};
+
+//usando predicado como função
+var fruta1 = frutas.Find(Procura);
+Console.WriteLine("Predicado => "+fruta1);
+
+//usando a expressão lambda
+var fruta2 = frutas.Find(i => i.Contains("n"));
+Console.WriteLine("Lambda => "+fruta2);
+
+Console.ReadKey();
+
+static bool Procura(string item)
+{
+    return item.Contains("n");
+}
